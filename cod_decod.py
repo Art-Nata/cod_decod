f04a1ca1694f7fa6c902a3798da2d75c0fd562ff
AHA <artemjukna@yandexlyceum.ru>
2023-11-24 13:24:31 +0300

    добавила новые функции кнопки дерево и отображения таблицы (не до конца) обновила окно - всё правильно

--- cod_decod.py
+++ cod_decod.py
@@ -1,7 +1,7 @@
 import sys
 
 from PyQt5 import uic
-from PyQt5.QtWidgets import QApplication, QMainWindow, QWidget
+from PyQt5.QtWidgets import QApplication, QMainWindow, QWidget, QTableWidgetItem
 
 
 def except_hook(cls, exception, traceback):
@@ -15,11 +15,12 @@ class Window_Code(QMainWindow):
         self.cod_button.clicked.connect(self.cod_mess)
         self.decod_button.clicked.connect(self.decod_mess)
         self.tableWidget.itemChanged.connect(self.item_changed)
+        self.tree_btn.clicked.connect(self.tree_code)
 
-        self.tableWidget.setColumnCount(8)
-        self.tableWidget.setRowCount(2)
+
         self.code_table = {
             'А': '0', 'Б': '00', 'В': '1', 'Г': '11', 'Д': '000'}
+        self.table_code()
 
     def cod_mess(self):
         message_new = self.lineEdit.text()
@@ -33,8 +34,22 @@ class Window_Code(QMainWindow):
         # Если значение в ячейке было изменено,
 
         self.code_table[self.titles[item.column()]] = item.text()
+
+
     def decod_mess(self):
         pass
+
+
+    def tree_code(self):
+        pass
+
+
+    def table_code(self):
+        self.tableWidget.setColumnCount(len(self.code_table))
+        for i in range(len(self.code_table)):
+            #self.tableWidget.setItem(0, i, QTableWidgetItem(str(self.code_table[i].key())))
+            print(self.code_table[i])
+
 
 
 if __name__ == '__main__':
